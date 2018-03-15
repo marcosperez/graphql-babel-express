d6c321dbd6ecde4ee2461034433b0ac6379a516c
Marcos Perez <marcos.d.perez@gmail.com>
2018-03-15 00:39:27 -0300

    se agrego  User.js

--- index.js
+++ index.js
@@ -5,52 +5,17 @@ import { importSchema } from "graphql-import";
 import { makeExecutableSchema } from "graphql-tools";
 
 const typeDefs = importSchema("./src/schemas/User.graphql");
-const resolvers = {};
+import userResolver from "./src/resolvers/User";
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
+const schema = makeExecutableSchema({ typeDefs });
 
-// This class implements the RandomDie GraphQL type
-class User {
-  constructor(
-    nombre = "marcos",
-    apellido = "perez",
-    edad = 0,
-    fechaNacimiento = ""
-  ) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.edad = edad || 999;
-    this.fechaNacimiento = fechaNacimiento;
-  }
-  id() {
-    return `${this.nombre}-${this.apellido}`;
-  }
-}
-let users = [];
 // The root provides the top-level API endpoints
-var root = {
-  getUser: function({ index }) {
-    return users[index];
-  },
-  saveUser: function({ input }) {
-    console.log(input);
-    const { nombre, apellido, edad, fechaNacimiento } = input;
-    const user = new User(nombre, apellido, edad, fechaNacimiento);
-    users.push(user);
-    console.log(JSON.stringify(users));
-    return user;
-  },
-  getUsers({ index }) {
-    return users;
-  }
-};
-
 var app = express();
 app.use(
   "/graphql",
   graphqlHTTP({
     schema: schema,
-    rootValue: root,
+    rootValue: userResolver,
     graphiql: true
   })
 );
