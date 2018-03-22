d63966c1a7005a52b731b80533a6272397fcb2a7
Marcos Perez <marcos.d.perez@gmail.com>
2018-03-22 00:13:22 -0300

    Test graphql
    Se creo el primer test de insercion de usuario. Se levanta un servidor graphql para cada insercion.

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export default {
+  APP_PORT: 4000,
+  GQL_URL_DIR: "/graphql"
+};
--- src/index.js
+++ src/index.js
@@ -1,23 +1,3 @@
-var express = require("express");
-var graphqlHTTP = require("express-graphql");
-import { makeExecutableSchema } from "graphql-tools";
-const glue = require("schemaglue");
+import * as graphqlServer from "./graphql/graphql";
 
-const { schema, resolver } = glue("src/graphql");
-
-const executableSchema = makeExecutableSchema({
-  typeDefs: schema,
-  resolvers: resolver
-});
-
-// The root provides the top-level API endpoints
-var app = express();
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: executableSchema,
-    graphiql: true
-  })
-);
-app.listen(4000);
-console.log("Running a GraphQL API server at localhost:4000/graphql");
+graphqlServer.start();
